6fe7099c3d0a0f31805ac756984749d79a681f01
Luke Barrier <lukebbarrier06@outlook.com>
2024-12-17 22:44:23 -0500

    Update

--- week-20-hw/mon/create_tables.sql
+++ week-20-hw/mon/create_tables.sql
@@ -6,22 +6,22 @@ DROP TABLE IF EXISTS bouquets;
 DROP TABLE IF EXISTS flower_types;
 
 CREATE TABLE flower_types (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(30) NOT NULL UNIQUE
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(30) NOT NULL UNIQUE
 );
 
 CREATE TABLE bouquets (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(40),
-  price NUMERIC(5, 2) NOT NULL
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(40),
+    price NUMERIC(5, 2) NOT NULL
 );
 
 CREATE TABLE flowers (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  color VARCHAR(20),
-  stem_length VARCHAR(10),
-  flower_type_id INTEGER NOT NULL,
-  bouquet_id INTEGER NOT NULL,
-  FOREIGN KEY (flower_type_id) REFERENCES flower_types(id) ON DELETE CASCADE,
-  FOREIGN KEY (bouquet_id) REFERENCES bouquets(id) ON DELETE CASCADE
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    color VARCHAR(20),
+    stem_length VARCHAR(10),
+    flower_type_id INTEGER NOT NULL,
+    bouquet_id INTEGER NOT NULL,
+    FOREIGN KEY (flower_type_id) REFERENCES flower_types(id) ON DELETE CASCADE,
+    FOREIGN KEY (bouquet_id) REFERENCES bouquets(id) ON DELETE CASCADE
 );
