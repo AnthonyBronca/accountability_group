998da347206269364d2f32318b29455a9ccb6a8d
root <root@MSI>
2024-12-18 16:57:48 -0800

    partial Monday Homework

--- week-20-hw/mon/create_tables.sql
+++ week-20-hw/mon/create_tables.sql
@@ -2,3 +2,16 @@
 PRAGMA foreign_keys = 1;
 
 -- Your code here
+DROP TABLE IF EXISTS bouquets;
+DROP TABLE IF EXISTS flower_types;
+
+CREATE TABLE bouquets (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50) NOT NULL,
+    price DECIMAL(2,2)   
+);
+
+CREATE TABLE flower_types (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50) NOT NULL
+);
